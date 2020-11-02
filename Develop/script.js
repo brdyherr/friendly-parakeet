bdb16318464175b7ed00b562be6c51ba1d5286d5
Brown Mamba <brdyherrera@gmail.com>
2020-11-01 21:43:53 -0500

    generate random password func

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,42 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", promptBox);
+
+function promptBox() {
+  var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+  //ask for character type
+  var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+}
+
+  // var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+  // //ask for character type
+  // var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+//prompt criteria
+function generatePassword() {
+  //evaluate character type
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } 
+  //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,6 +48,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// alert(generatePassword());
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// if (selectedCharacters === null || selectedCharacters === "") {
+//   window.alert("You must select at least one character type. Please try again.");
+//   return generatePassword();
+// }
